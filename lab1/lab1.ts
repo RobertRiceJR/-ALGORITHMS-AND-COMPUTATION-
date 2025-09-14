bd558a762519ed6b4f8c235f50132bd14559ecc0
Robert Rice <robertrice12009@yahoo.com>
2025-09-14 06:33:54 -0500

    Updates to Folder

--- lab1/lab1.ts
+++ lab1/lab1.ts
@@ -1,21 +1,19 @@
 function numTilings(n: number): number {
     const mod = 1000000007;
-    // dp[i] will store the number of tilings for a 2 x i board.
+    // dp[i] to store the number of tilings for a 2 x i board.
     const dp: number[] = new Array(n + 1).fill(0);
     
-    // Base cases:
-    dp[0] = 1; 
+    // base cases
+    dp[0] = 1;  
     dp[1] = 1;
     if(n >= 2) {
         dp[2] = 2;
     }
     
-    // Build the dp table using the recurrence:
+    //table using the recurrence:
     for (let i = 3; i <= n; i++) {
         dp[i] = (2 * dp[i - 1] + dp[i - 3]) % mod;
     }
-    console.log(dp[n])
+    
     return dp[n];
 }
-
-numTilings(5)
